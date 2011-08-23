b4b9e5f521c01401e588841d361056c3ba093c5a
wbond <will@wbond.net>
2011-08-23 13:24:28 -0400

    Fixed support for Windows, added support for https github clones, improved performance by switching webbrowser module usage over to Sublime Text 2 open_url command

--- githubinator.py
+++ githubinator.py
@@ -1,6 +1,5 @@
 import os
 import re
-import webbrowser
 import sublime
 import sublime_plugin
 
@@ -11,65 +10,56 @@ class GithubinatorCommand(sublime_plugin.TextCommand):
     '''
 
     def run(self, edit):
-        if len(self.view.file_name()) > 0:
-            full_name = self.view.file_name()
-            folder_name, file_name = os.path.split(full_name)
-            begin_line, begin_column = self.view.rowcol(self.view.sel()[0].begin())
-            end_line, end_column = self.view.rowcol(self.view.sel()[0].end())
-            begin_line = str(begin_line + 1)
-            end_line = str(end_line + 1)
+        if not self.view.file_name():
+            return
 
-            git_path = self.recurse_dir(folder_name,'.git')
-            if git_path:
-                git_config_path = os.path.join(git_path,'.git','config')
-                new_git_path = folder_name[len(git_path):]           
+        full_name = self.view.file_name()
+        folder_name, file_name = os.path.split(full_name)
 
-                git_config_file = open(git_config_path, "r")
-                config = git_config_file.read()
-                git_config_file.close()
-                config = str(config)
+        git_path = self.recurse_dir(folder_name,'.git')
+        if not git_path:
+            sublime.status_message('Could not find .git directory.')
+            print 'Could not find .git directory.'
+            return
 
-                mainline = re.search(r'mainline.*\s.*:(.*)/(.*)\.', config)
-                origin = re.search(r'origin.*\s.*:(.*)/(.*)\.', config)
-                gitrdone = None
+        git_config_path = os.path.join(git_path,'.git','config')
+        new_git_path = folder_name[len(git_path):]
 
-                if mainline:
-                    gitrdone = list(mainline.groups())
-                elif origin:
-                    gitrdone = list(origin.groups())
-                else:
-                    pass
+        with open(git_config_path, "r") as git_config_file:
+            config = git_config_file.read()
 
-                if gitrdone:
-                    gitrdone[:] = ['/'.join(gitrdone[:])]
-                    gitrdone = str(gitrdone).strip("[']")
-
-                if begin_line == end_line:
-                    lines = begin_line
-                else:
-                    lines = '%s-%s' % (begin_line, end_line)
-
-            if git_path and gitrdone:
-                for region in self.view.sel():
-                    git_link = 'https://github.com/%s/blob/master%s' % (gitrdone, new_git_path)
-                    full_link = '%s/%s#L%s' % (git_link, file_name, lines)
-                    sublime.set_clipboard(full_link)
-                    sublime.status_message('Copied %s to clipboard.' % full_link)
-                    print 'Copied %s to clipboard.' % full_link
-                    webbrowser.open_new_tab(full_link)
-            else:
-                sublime.status_message('Could not find .git directory.')
-                print 'Could not find .git directory.'
+        sel = self.view.sel()[0]
+        begin_line = self.view.rowcol(sel.begin())[0] + 1
+        end_line = self.view.rowcol(sel.end())[0] + 1
+        if begin_line == end_line:
+            lines = begin_line
         else:
-            pass
+            lines = '%s-%s' % (begin_line, end_line)
+        
+        for remote in ['mainline', 'origin']:
+            regex = r'.*\s.*(?:https://github\.com/|github\.com:)(.*)/(.*?)(?:\.git)?\r?\n'
+            result = re.search(remote + regex, config)
+            if not result:
+                continue
+            matches = result.groups()
+
+            full_link = 'https://github.com/%s/%s/blob/master%s/%s#L%s' % \
+                (matches[0], matches[1], new_git_path, file_name, lines)
+            sublime.set_clipboard(full_link)
+            sublime.status_message('Copied %s to clipboard.' % full_link)
+            print 'Copied %s to clipboard.' % full_link
+            self.view.window().run_command('open_url', {"url": full_link})
+            break
 
 
     def recurse_dir(self, path, folder):
         items = os.listdir(path)
         if folder in items and os.path.isdir(os.path.join(path, folder)):
             return path
-        else:
-            return self.recurse_dir(os.path.dirname(path), folder) if '/' != path else None
+        dirname = os.path.dirname(path)
+        if dirname == path:
+            return None
+        return self.recurse_dir(dirname, folder)
 
 
     def is_enabled(self):
